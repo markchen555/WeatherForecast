c72d49fa69e6c84a4c0740040c44e9aeeb10eadb
Mark Chen <markchen555@gmail.com>
2018-01-19 18:57:19 -0800

    Doc: connect search_bar with redux and imported fetchWeather action

--- README.md
+++ README.md
@@ -20,3 +20,10 @@ Checkout this repo, install dependencies, then start the gulp process with the f
 > npm install
 > npm start
 ```
+
+#### Definition of each Redux components
+
+- middleware is a function that take an action and depending on the action's type and the action's payload or any other numbers of factors, the middleware can choose to let the action pass through, it can manipulate the action, it can console.log it or it can stop all together. it can do all these different type of little tasks on these actions before they reach any reduser.
+
+
+#### Steps on implement Redux
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
 
-    this.onInputChange = this.onInputChange.bind(this)
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -15,6 +19,10 @@ export default class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
+
+    // we need to go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   // If you create a search bar out of a form element,
@@ -37,3 +45,11 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// connect's first argument is for mapStatesToProps and second argument is for mapDispatchToProps
+// If there is no mapStatesToProps, pass in null.
+export default connect(null, mapDispatchToProps)(SearchBar);
